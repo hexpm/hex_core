c525d8936202d2e8fc3622a38b31b8f5f277928d
Wojtek Mach <wojtek@wojtekmach.pl>
2018-03-11 02:01:55 +0100

    Add erts version to User-Agent

--- src/hex_http.erl
+++ src/hex_http.erl
@@ -1,5 +1,8 @@
 -module(hex_http).
 -export([get/3]).
+-ifdef(TEST).
+-export([user_agent/1]).
+-endif.
 -include_lib("hex_erl.hrl").
 
 -type client() :: #{adapter => module(), user_agent_string => string()}.
@@ -16,8 +19,10 @@ get(#{adapter := Adapter, user_agent_string := UserAgentString}, URI, Headers) -
     Adapter:get(URI, Headers2).
 
 user_agent(UserAgentString) ->
-    OtpVersion = erlang:system_info(otp_release),
-    "hex_erl/" ++ ?HEX_ERL_VERSION ++ " " ++ UserAgentString ++ " (OTP/" ++ OtpVersion ++ ")".
+    OTPRelease = erlang:system_info(otp_release),
+    ERTSVersion = erlang:system_info(version),
+    OTPString = " (OTP/" ++ OTPRelease ++ " erts-" ++ ERTSVersion ++ ")",
+    "hex_erl/" ++ ?HEX_ERL_VERSION ++ " " ++ UserAgentString ++ OTPString.
 
 put_new(Key, Value, Map) ->
     case maps:find(Key, Map) of
