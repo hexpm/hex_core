7ef3278c206be24ad097a3a8e9d189895c37fd63
Wojtek Mach <wojtek@wojtekmach.pl>
2018-04-19 21:53:39 +0200

    Move hex.pm public key to the top of module

--- src/hex_repo.erl
+++ src/hex_repo.erl
@@ -10,6 +10,16 @@
     get_tarball/2,
     get_tarball/3
 ]).
+%% https://hex.pm/docs/public_keys
+-define(HEXPM_PUBLIC_KEY, <<"-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApqREcFDt5vV21JVe2QNB
+Edvzk6w36aNFhVGWN5toNJRjRJ6m4hIuG4KaXtDWVLjnvct6MYMfqhC79HAGwyF+
+IqR6Q6a5bbFSsImgBJwz1oadoVKD6ZNetAuCIK84cjMrEFRkELtEIPNHblCzUkkM
+3rS9+DPlnfG8hBvGi6tvQIuZmXGCxF/73hU0/MyGhbmEjIKRtG6b0sJYKelRLTPW
+XgK7s5pESgiwf2YC/2MGDXjAJfpfCd0RpLdvd4eRiXtVlE9qO9bND94E7PgQ/xqZ
+J1i2xWFndWa6nfFnRxZmCStCOZWYYPlaxr+FZceFbpMwzTNs4g3d4tLNUcbKAIH4
+0wIDAQAB
+-----END PUBLIC KEY-----">>).
 
 -type options() :: [{client, client()} | {repo, repo()} | {verify, boolean()}|
                     {etag, etag()}     | {cache_dir, file:filename_all()}].
@@ -26,18 +36,8 @@
 %% @end
 -spec default_options() -> options().
 default_options() ->
-    %% https://hex.pm/docs/public_keys
-    PublicKey = <<"-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApqREcFDt5vV21JVe2QNB
-Edvzk6w36aNFhVGWN5toNJRjRJ6m4hIuG4KaXtDWVLjnvct6MYMfqhC79HAGwyF+
-IqR6Q6a5bbFSsImgBJwz1oadoVKD6ZNetAuCIK84cjMrEFRkELtEIPNHblCzUkkM
-3rS9+DPlnfG8hBvGi6tvQIuZmXGCxF/73hU0/MyGhbmEjIKRtG6b0sJYKelRLTPW
-XgK7s5pESgiwf2YC/2MGDXjAJfpfCd0RpLdvd4eRiXtVlE9qO9bND94E7PgQ/xqZ
-J1i2xWFndWa6nfFnRxZmCStCOZWYYPlaxr+FZceFbpMwzTNs4g3d4tLNUcbKAIH4
-0wIDAQAB
------END PUBLIC KEY-----">>,
     Client = #{adapter => hex_http_httpc, user_agent_fragment => <<"(httpc)">>},
-    Repo = #{uri => <<"https://repo.hex.pm">>, public_key => PublicKey},
+    Repo = #{uri => <<"https://repo.hex.pm">>, public_key => ?HEXPM_PUBLIC_KEY},
     [{client, Client}, {repo, Repo}, {verify, true}].
 
 %% @doc
