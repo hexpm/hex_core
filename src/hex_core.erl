eb225f4d05130afed0dc797fdb68cacd5735843f
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2019-02-21 16:19:21 +0100

    Document config options

--- src/hex_core.erl
+++ src/hex_core.erl
@@ -1,3 +1,39 @@
+%% @doc
+%% hex_core entrypoint module.
+%%
+%% ### Config
+%%
+%% Most functions in the hex_core API takes a configuration. The configuration sets things
+%% like HTTP client to use, and API and repository URL. Some of these configuration options
+%% will likely be static for your application and some may change depending on the function
+%% you call.
+%%
+%% ##### Options
+%%
+%% * `api_key' - Authentication key used when accessing the HTTP API.
+%% * `api_organization' - Name of the organization endpoint in the API, this should
+%%   for example be set when accessing key for a specific organization.
+%% * `api_repository' - Name of the repository endpoint in the API, this should
+%%   for example be set when accessing packages from a specific repository.
+%% * `api_url' - URL to the HTTP API (default: `https://hex.pm/api').
+%% * `http_adapter' - Callback module used for HTTP requests, see [`hex_http'](hex_http.html)
+%%   (default: `hex_http_httpc').
+%% * `http_etag' - Sets the `if-none-match' HTTP header with the given value to do a
+%%   conditional HTTP request.
+%% * `http_adapter_config' - Configuration to pass to the HTTP adapter.
+%% * `http_user_agent_fragment' - Will be appended to the `user-agent` HTTP header (default: `(httpc)').
+%% * `repo_key' - Authentication key used when accessing the repository.
+%% * `repo_name' - Name of the repository, used for verifying the repository signature
+%%   authenticity (default: `hexpm').
+%% * `repo_public_key' - Public key used to verify the repository signature
+%%   (defaults to hexpm public key `https://hex.pm/docs/public_keys').
+%% * `repo_url' - URL to the repository (default: `https://repo.hex.pm').
+%% * `repo_organization' - Name of the organization repository, appends `/repos/:name'
+%%    to the repository URL.
+%% * `repo_verify' - If `true' will verify the repository signature (default: `true').
+%% * `repo_verify_origin' - If `true' will verify the repository signature origin,
+%%   requires protobuf messages as of hex_core v0.4.0 (default: `true').
+
 -module(hex_core).
 
 -include("hex_core.hrl").
@@ -20,9 +56,9 @@ J1i2xWFndWa6nfFnRxZmCStCOZWYYPlaxr+FZceFbpMwzTNs4g3d4tLNUcbKAIH4
 
 -type config() :: #{
     api_key => binary() | undefined,
-    api_url => binary(),
-    api_repository => binary() | undefined,
     api_organization => binary() | undefined,
+    api_repository => binary() | undefined,
+    api_url => binary(),
     http_adapter => module(),
     http_etag => binary() | undefined,
     http_adapter_config => map(),
@@ -40,9 +76,9 @@ J1i2xWFndWa6nfFnRxZmCStCOZWYYPlaxr+FZceFbpMwzTNs4g3d4tLNUcbKAIH4
 default_config() ->
     #{
         api_key => undefined,
-        api_url => <<"https://hex.pm/api">>,
-        api_repository => undefined,
         api_organization => undefined,
+        api_repository => undefined,
+        api_url => <<"https://hex.pm/api">>,
         http_adapter => hex_http_httpc,
         http_adapter_config => #{profile => default},
         http_etag => undefined,
