5fe38dec340c6de69cb19a3c4dc1c09d7b542ca7
Bryan Paxton <bryan@payitoff.io>
2019-12-03 21:01:49 -0600

    address PR feedback

--- src/hex_api_auth.erl
+++ src/hex_api_auth.erl
@@ -1,9 +1,5 @@
 -module(hex_api_auth).
--export([
-    test_key/3
-]).
-
--define(ERL_CONTENT_TYPE, <<"application/vnd.hex+erlang">>).
+-export([test/2]).
 
 %% @doc
 %% Test an auth key against a given domain and resource.
@@ -11,16 +7,14 @@
 %% Examples:
 %%
 %% ```
-%% 1> Params = [{domain, <<"repository">>}, {resource, <<"gustafson_motors">>}].
-%% [{domain,<<"repository">>},
-%% {resource,<<"gustafson_motors">>}]
-%% 2> Key = <<"86c4ac12292843b66fdee41e022f8497">>
-%% 2> hex_api_auth:test_key(hex_core:default_config(), Params, Key).
+%% 1> Params = #{domain, <<"repository">>, {resource, <<"gustafson_motors">>}}.
+%% #{domain => <<"repository">>,
+%%   resource => <<"gustafson_motors">>}
+%% 2> hex_api_auth:test_key(hex_core:default_config(), Params).
 %% {ok,{204, ..., nil}}
 %% '''
 %% @end
--spec test_key(map(), map(),  binary()) -> hex_api:response().
-test_key(Repo, Params, Key) ->
-    ParamList = maps:to_list(Params),
-    URI = ["auth?", hex_api:encode_query_string(ParamList)],
-    hex_api:get(Repo#{api_key => Key}, list_to_binary(URI)).
+-spec test(hex_core:config(), map()) -> hex_api:response().
+test(Config, #{domain := Domain, resource := Resource}) ->
+    URI = ["auth", "?domain=", Domain, "&resource=", Resource],
+    hex_api:get(Config, list_to_binary(URI)).
