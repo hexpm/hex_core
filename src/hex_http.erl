4501627a413718e5804d148fab3cd2bc61081517
Pablo Costas Sánchez <pablo.costas.sanchez@gmail.com>
2020-10-25 12:00:02 +0100

    Warn of `http_adapter_config` deprecation

--- src/hex_http.erl
+++ src/hex_http.erl
@@ -18,7 +18,16 @@
 -spec request(hex_core:config(), method(), URI :: binary(), headers(), body()) ->
     {ok, {status(), headers(), binary()}} | {error, term()}.
 request(Config, Method, URI, Headers, Body) when is_binary(URI) and is_map(Headers) ->
-    {Adapter, AdapterConfig} = maps:get(http_adapter, Config, {hex_http_httpc, #{}}),
+    {Adapter, AdapterConfig} = case maps:get(http_adapter, Config, {hex_http_httpc, #{}}) of
+        {Adapter0, AdapterConfig0} ->
+            {Adapter0, AdapterConfig0};
+        %% TODO: remove in v0.9
+        Adapter0 when is_atom(Adapter0) ->
+            AdapterConfig0 = maps:get(http_adapter_config, Config, #{}),
+            io:format("[hex_http] setting #{http_adapter => Module, http_adapter_config => Map} "
+                      "is deprecated in favour of #{http_adapter => {Module, Map}}~n"),
+            {Adapter0, AdapterConfig0}
+    end,
     UserAgentFragment = maps:get(http_user_agent_fragment, Config),
     Headers2 = put_new(<<"user-agent">>, user_agent(UserAgentFragment), Headers),
     Adapter:request(Method, URI, Headers2, Body, AdapterConfig).
