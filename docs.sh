9b3a61943b0aee1ef812c1de305a2e2fd3b9ddee
Wojtek Mach <wojtek@wojtekmach.pl>
2021-02-12 10:20:59 +0100

    Update docs configuration

--- docs.sh
+++ docs.sh
@@ -4,34 +4,12 @@ set -e
 # Setup:
 #
 #     mix escript.install github elixir-lang/ex_doc
-#     OTP_GITHUB_URL="https://github.com/erszcz/otp" asdf install erlang ref:edoc-chunk-support
-#     asdf local erlang ref:edoc-chunk-support
+#     asdf install erlang ref:master
+#     asdf local erlang ref:master
 
+rebar3 compile
 rebar3 edoc
 version=0.7.1
 ex_doc "hex_core" $version "_build/default/lib/hex_core/ebin" \
   --source-ref v${version} \
   --config docs.exs $@
-
-# Work around rebar3 docs publishing bug, define fake Mix project:
-#
-#     # mix.exs
-#     defmodule HexCore.MixProject do
-#       use Mix.Project
-#
-#       def project() do
-#         [
-#           app: :hex_core,
-#           version: "0.7.1",
-#           aliases: [
-#             docs: fn _ ->
-#               :ok
-#             end
-#           ]
-#         ]
-#       end
-#     end
-#
-# And run:
-#
-#     mix hex.publish docs
