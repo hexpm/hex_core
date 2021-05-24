d1f7be4cd4f66eb783af6f9484cd4ca011663185
Wojtek Mach <wojtek@wojtekmach.pl>
2021-05-24 19:26:33 +0200

    Trigger warning in hex_http_httpc:request/5

--- src/hex_http_httpc.erl
+++ src/hex_http_httpc.erl
@@ -1,5 +1,15 @@
 %% @doc
 %% httpc-based implementation of `hex_http' contract.
+%%
+%% Configuration keys:
+%%
+%% * `profile' - the name of the profile, defaults to `default'. See <a
+%% href="http://erlang.org/doc/man/httpc.html#set_options-2">`httpc:set_options/2'</a>
+%% for more information on setting options on profiles.
+%%
+%% * `http_options' - a list of HTTP options, defaults to `[]'. See <a
+%% href="http://erlang.org/doc/man/httpc.html#request-5">`httpc:request/5'</a>
+%% for a list of available HTTP options.
 
 -module(hex_http_httpc).
 -behaviour(hex_http).
@@ -11,8 +21,19 @@
 
 request(Method, URI, ReqHeaders, Body, AdapterConfig) ->
     Profile = maps:get(profile, AdapterConfig, default),
+    HTTPOptions = maps:get(http_options, AdapterConfig, []),
+
+    case proplists:get_value(ssl, HTTPOptions) of
+      undefined ->
+        io:format("[hex_http_httpc] using default ssl options which are insecure.~n"
+                  "Configure your adapter with: "
+                  "{hex_http_httpc, #{http_options => [{ssl, SslOpts}]}}~n");
+      _ ->
+        ok
+    end,
+
     Request = build_request(URI, ReqHeaders, Body),
-    case httpc:request(Method, Request, [], [{body_format, binary}], Profile) of
+    case httpc:request(Method, Request, HTTPOptions, [{body_format, binary}], Profile) of
         {ok, {{_, StatusCode, _}, RespHeaders, RespBody}} ->
             RespHeaders2 = load_headers(RespHeaders),
             {ok, {StatusCode, RespHeaders2, RespBody}};
