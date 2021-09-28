9e04defde36b109f99aae835d1ae3544143d797f
Todd Resudek <toddresudek@gmail.com>
2021-09-27 22:40:30 -0600

    Adds licenses module that can be vendored includes test

---
+++ src/hex_licenses.erl
@@ -0,0 +1,30 @@
+%% @doc
+%% Functions for determine license validatity.
+
+-module(mix_hex_licenses).
+
+-define(CANONICAL_LICENSES, load_licenses()).
+
+-export([licenses/0]).
+-export([invalid_licenses/1]).
+
+load_licenses() -> 
+    [CanonicalLicenses] = licenses(),
+    CanonicalLicenses.
+
+licenses() ->
+    {ok, Contents} = file:consult("src/licenses.erlterm"),
+    Contents.
+
+invalid_licenses(UserLicenseList) ->
+    CanonicalLicenseIDs = license_ids_from_license_list(?CANONICAL_LICENSES, gb_sets:new()),
+    InvalidLicenses =
+        lists:filter(fun(UserLicense) -> not gb_sets:is_element(UserLicense, CanonicalLicenseIDs)
+                     end,
+                     UserLicenseList),
+    InvalidLicenses.
+
+license_ids_from_license_list ([], Acc) -> Acc; 
+license_ids_from_license_list ([#{license_id := LicenseID} | T] , Acc) -> 
+    NewAcc = gb_sets:add_element(LicenseID, Acc),
+    license_ids_from_license_list (T, NewAcc).
---
+++ test/hex_licenses_SUITE.erl
@@ -0,0 +1,18 @@
+-module(hex_licenses_SUITE).
+
+-compile([export_all]).
+
+-include_lib("eunit/include/eunit.hrl").
+-include_lib("kernel/include/file.hrl").
+-include_lib("common_test/include/ct.hrl").
+
+all() ->
+    [licenses_test, invalid_licenses_test].
+
+licenses_test(_Config) ->
+    [] = hex_licenses:licenses(),
+    ok.
+
+invalid_licenses_test(_Config) ->
+    [] = hex_licenses:invalid_licenses(["MIT"]),
+    ok.
