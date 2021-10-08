19fac90cb84499af2c4fce851707cf6ef1e061f0
Todd Resudek <toddresudek@gmail.com>
2021-10-08 10:23:03 -0600

    Adds licenses module that can be vendored (#128)
    * Adds licenses module that can be vendored
    * sorted by license id

---
+++ test/hex_licenses_SUITE.erl
@@ -0,0 +1,18 @@
+-module(hex_licenses_SUITE).
+
+-compile([export_all]).
+
+-include_lib("eunit/include/eunit.hrl").
+-include_lib("kernel/include/file.hrl").
+-include_lib("common_test/include/ct.hrl").
+
+all() ->
+    [license_test, invalid_license_test].
+
+license_test(_Config) ->
+    true = hex_licenses:valid(<<"MIT">>),
+    ok.
+
+invalid_license_test(_Config) ->
+    false = hex_licenses:valid(<<"MICROSOFT">>),
+    ok.
